237befbc9053fa50d0d04b9ab5aa153b9e4ba678
rudnaid <david.rudnai@gmail.com>
2025-08-04 13:55:16 +0200

    fix(Move): refactor Move and Position to be structs

--- include/core/Move.h
+++ include/core/Move.h
@@ -1,18 +1,7 @@
 #pragma once
 #include "Position.h"
-#include "GameState.h"
 
-class Move {
+struct Move {
     Position from;
     Position to;
-
-public:
-    Move(const Position& from, const Position& to);
-
-    void execute(GameState& state);
-    void undo(GameState& state);
-    bool isValid(GameState& state) const;
-
-    [[nodiscard]] const Position& getFrom() const;
-    [[nodiscard]] const Position& getTo() const;
 };
--- include/core/Position.h
+++ include/core/Position.h
@@ -1,14 +1,6 @@
 #pragma once
-#include <vector>
 
-class Position {
+struct Position {
   int row;
   int col;
-public:
-  Position(int row, int col);
-  [[nodiscard]] bool isOnBoard() const;
-  [[nodiscard]] std::vector<Position> neighbors() const;
-  [[nodiscard]] int getRow() const;
-  [[nodiscard]] int getCol() const;
-  bool operator==(const Position& other) const;
 };
