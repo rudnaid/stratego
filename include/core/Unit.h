d89d5a143421dee9458e7687072be4a1c0c4cb16
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-08-05 11:13:30 +0200

    add getters to Unit class

--- include/core/Unit.h
+++ include/core/Unit.h
@@ -2,7 +2,15 @@
 #include "PieceType.h"
 
 class Unit {
-  PieceType type;
-  Player owner;
+  PieceType& type;
+  Player& owner;
+  IMovementStrategy& moveStrategy;
+  ICombatStrategy& combatStrategy;
 
+public:
+  Unit(PieceType& type, Player& owner, ICombatStrategy& combatStrategy, IMoveStrategy& moveStrategy);
+
+  int getPower() const;
+  Player& getOwner() const;
+  std::string getName() const;
 };
