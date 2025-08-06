8a609b13fb1c34d552fd0752a79a11ef82b59688
rudnaid <david.rudnai@gmail.com>
2025-08-06 12:35:50 +0200

    feat(GameConfig): add PIECE_TYPES map to GameConfig for Unit creation

--- source/core/GameConfig.cpp
+++ source/core/GameConfig.cpp
@@ -5,3 +5,18 @@ const std::vector<Position> &GameConfig::LAKE_POSITIONS() {
                                               {4, 6}, {4, 7}, {5, 6}, {5, 7}};
   return lakes;
 }
+
+const std::map<std::string, PieceType> GameConfig::PIECE_TYPES = {
+  { "Marshal",    PieceType(UnitRank::Marshal,    10, 1) },
+  { "General",    PieceType(UnitRank::General,     9, 1) },
+  { "Colonel",    PieceType(UnitRank::Colonel,     8, 2) },
+  { "Major",      PieceType(UnitRank::Major,       7, 3) },
+  { "Captain",    PieceType(UnitRank::Captain,     6, 4) },
+  { "Lieutenant", PieceType(UnitRank::Lieutenant,  5, 4) },
+  { "Sergeant",   PieceType(UnitRank::Sergeant,    4, 4) },
+  { "Miner",      PieceType(UnitRank::Miner,       3, 5) },
+  { "Scout",      PieceType(UnitRank::Scout,       2, 8) },
+  { "Spy",        PieceType(UnitRank::Spy,         1, 1) },
+  { "Bomb",       PieceType(UnitRank::Bomb,        0, 6) },
+  { "Flag",       PieceType(UnitRank::Flag,        0, 1) }
+};
